623819653df97cbc714605afcf2e6e884b00dcde
Sam Schultz <sam.schultz@outlook.com>
2022-08-23 11:53:13 -0400

    Creating_A_Database_ClassLesson_Db_Bootcamp

--- Creating_A_Database_ClassLesson.sql
+++ Creating_A_Database_ClassLesson.sql
@@ -18,7 +18,7 @@ Create Database Bootcamp;
 
 Use Bootcamp;                                                   --Want to be in the database where you will be making new tables, this ensures that
 
-create table Studetns (
+create table Students (
     StudentId int not null primary key identity(1, 1),         --The Identity function creates a unique key for the Primary Key
     FirstName varchar(30) not null,
     LastName Varchar(60) not null,
@@ -27,4 +27,11 @@ create table Studetns (
     Phone varchar(60) not null,
     Email varchar(100) null,
     DateOfBirth Date not null
+);
+Create Table GraduationRequirements (
+    Id int not null primary key identity(1, 2),                                                 --You only change the second digit in the Identity Function
+    Student int not null FOREIGN key References Students(StudentId),                          --Foreign key must match primary key data type and info, keep same name. Is allowed to be null. In this case it would not be null becayse that data will be linked with a student. Must refrence the primary key
+    Topic varchar(60) not null,
+    Score Int null,
+    DateTaken date null,
 );
